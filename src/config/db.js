8fc68ff04be153f944176d6b45ed92802c2bdea3
aalimkhan14 <aalimmuradia4@gmail.com>
2025-12-24 02:27:22 +0430

    Refactor code structure for improved readability and maintainability

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,25 @@
-const {Sequelize} = require('sequelize');
+// const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize(
-    'neyazi_school',
-    'root',
-    '1234',
-    {
-        host: 'localhost',
-        dialect: 'mysql'
-    }
-)
-module.exports = sequelize
+// const sequelize = new Sequelize(
+//     'neyazi_school',
+//     'root',
+//     '1234',
+//     {
+//         host: 'localhost',
+//         dialect: 'mysql'
+//     }
+// )
+// module.exports = sequelize
+
+
+
+const { Sequelize } = require('sequelize');
+const path = require('path');
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, '../database/school.sqlite'),
+    logging: false
+});
+
+module.exports = sequelize;
